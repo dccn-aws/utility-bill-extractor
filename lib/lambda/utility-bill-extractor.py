1c7986d509e20e460bbe72a1feb3886ec15e5d67
dccn-aws <dccn@amazon.com>
2022-09-18 22:47:12 +0000

    modified code to integrate better with workshop. still testing

--- lib/lambda/utility-bill-extractor.py
+++ lib/lambda/utility-bill-extractor.py
@@ -3,6 +3,7 @@ import time
 import urllib.parse
 import boto3
 import logging
+import re
 from datetime import datetime
 
 LOGGER = logging.getLogger()
@@ -13,34 +14,33 @@ LOGGER.info('Boto3 Version: {0}'.format(boto3.__version__))
 
 s3 = boto3.client('s3')
 textract = boto3.client('textract')
-ddb = boto3.resource('dynamodb') # UtilityBillDataTable
+awslambda = boto3.client('lambda')
 
-def query_S3document(bucket, key, query):
+RAW_DATA_BUCKET = 'carbonlake-bills-storage'
+TRANSFORMED_DATA_BUCKET = 'carbonlake-transformed-data'
+
+def query_Textract(bills, query):
     # Event is lambda event
     # query is a QueriesConfig object for textract.start_document_analysis()
-    textract_response = textract.start_document_analysis(
-            DocumentLocation={
-                'S3Object': {
-                    'Bucket': bucket,
-                    'Name': key
-                }
-            },
-            FeatureTypes=['QUERIES'],
-            QueriesConfig=query
-        )
+    jobs = []
+    for bill in bills:
+        textract_response = textract.start_document_analysis(
+                DocumentLocation={
+                    'S3Object': {
+                        'Bucket': RAW_DATA_BUCKET,
+                        'Name': bill
+                    }
+                },
+                FeatureTypes=['QUERIES'],
+                QueriesConfig=query
+            )
         
-    job = textract_response['JobId']
+        job = textract_response['JobId']
+        jobs.append(job)
+        time.sleep(.1) # hardcoded throttling to avoid tripping Textract Quotas
     
-    textract_response = textract.get_document_analysis(JobId=job)
-    while(textract_response["JobStatus"] == "IN_PROGRESS"):
-        time.sleep(1)
-        textract_response = textract.get_document_analysis(JobId=job)
-
-    lookup = ["QUERY", "QUERY_RESULT"]
-    
-    queryResults = [b for b in textract_response["Blocks"] if b['BlockType'] in lookup]
-    
-    return queryResults
+    LOGGER.info('Querying {0} bills in Textract'.format(len(jobs)))
+    return jobs
     
 def unpack_query(results):
     i = 0
@@ -49,61 +49,121 @@ def unpack_query(results):
         data[results[i]['Query']['Alias']] = results[i+1]['Text']
         i = i+2
     
+    # Try block to catch multiple datetime formats, you may need to add more here
     for schema in ['%B %d,%Y', '%B %d, %Y']:
         try:
-            data['Bill_Date'] = str(datetime.strptime(data['Bill_Date'],schema))
+            data['bill_date'] = datetime.strptime(data['bill_date'],schema).strftime('%Y-%m-%d')
         except:
             pass
         
     return data
+    
+def retrieve_Textract_query_results(jobids):
+    results = []
+    for job in jobids:
+        textract_response = textract.get_document_analysis(JobId=job)
+        while(textract_response["JobStatus"] == "IN_PROGRESS"):
+            time.sleep(1) if textract_response["JobStatus"] == "IN_PROGRESS"
+            textract_response = textract.get_document_analysis(JobId=job)
+    
+        lookup = ["QUERY", "QUERY_RESULT"]
+        
+        queryResults = [b for b in textract_response["Blocks"] if b['BlockType'] in lookup]
+        
+        results.append(unpack_query(queryResults))
+    
+    LOGGER.info('Successfully retrieved {0} queries from Textract'.format(len(jobids)))
+    return results
+    
+def get_bills(bucket):
+    
+    return bills
+    
+def transform_data(data):
+    TransformedData = []
+    for entry in data:
+        event_id = '{0}-{1}-{2}'.format(entry['utility_provider_name'],
+                                        entry['es_id'],
+                                        entry['bill_date']
+                                        )
+        zipcode = re.search("\d{5}\-?\d{0,4}", entry['utility_provider_address']).group()
+        activity = awslambda.invoke(FunctionName='GridRegionSelector',
+                                    Payload={'country':'US','zipcode':zipcode},
+                                    RequestType='RequestResponse'
+                                    )
+        TransformedData.append({
+            "activity_event_id": event_id,
+            "supplier": entry['utility_provider_name'],
+            "scope": 2,
+            "category": "grid-region-location-based",
+            "activity": entry['utility_provider_name'],
+            "raw_data": entry['kwh_usage'],
+            "units": "kwH"
+            }
+        )
+        
+    return TransformedData
+    
+def write_bills_to_s3(bills):
+    
+    for bill in bills:
+        filename = "{0}.pdf".format(bill['activity_event_id'])
+        with open(filename, "w") as outfile:
+            json.dump(bill, outfile)
+        s3.put_ojbect(Body=filename,
+                      Bucket=TRANSFORMED_DATA_BUCKET,
+                      key='scope2-bill-extracted-data/{0}'.format(filename))
+                      )
+        
+        
 
 def lambda_handler(event, context):
+    
+    resp = s3.list_objects_v2(Bucket=RAW_DATA_BUCKET, Prefix="utility-bills/")
+    bills = [b['Key'] for b in resp['Contents'] if '.pdf' in b['Key']]
 
-    # Get the object from the event notification and send it to Textract
-    bucket = event['Records'][0]['s3']['bucket']['name']
-    key = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'], encoding='utf-8')
     query = {
                 "Queries":[
                     {
                         "Text": "What is this customer's name?",
-                        "Alias": "Customer_Name"
+                        "Alias": "customer_name"
                     },
                     {
                         "Text": "What is this customer's address?",
-                        "Alias": "Customer_Address"
+                        "Alias": "customer_address"
                     },
                     {
                         "Text": "What is the meter ID?",
-                        "Alias": "ES_ID"
+                        "Alias": "es_id"
                     },
                     {
                         "Text": "How many kWhs were used?",
-                        "Alias": "kWh_Usage"
+                        "Alias": "kwh_usage"
                     },
                     {
                         "Text": "What is the statement date?",
-                        "Alias": "Bill_Date"
+                        "Alias": "bill_date"
                     },
                     {
                         "Text": "What's the name at the top of the bill?",
-                        "Alias": "Utility_Provider_Name"
+                        "Alias": "utility_provider_name"
                     },
                     {
                         "Text": "What's the address at the top of the bill?",
-                        "Alias": "Utility_Provider_Address"
+                        "Alias": "utility_provider_address"
                     },
                 ]
                 
             }
     
-    queryResults = query_S3document(bucket, key, query)
+    TextractJobs = query_Textract(bills, query)
+    
+    query_results = retrieve_Textract_query_results(TextractJobs)
+    
+    ExtractedData = unpack_query(query_results)
+    
+    TransformedBills = transform_data(ExtractedData)
+    
+    write_bills_to_s3(TransformedBills)
+        return data
 
-    data = unpack_query(queryResults)
-    
-    utilityBillTable = ddb.Table('UtilityBillDataTable')
-    
-    utilityBillTable.put_item(Item=data)
-    
-    LOGGER.info('Query contains {0} results, cleaned: {1}'.format(len(queryResults)/2,json.dumps(data)))
-    
-    return data
