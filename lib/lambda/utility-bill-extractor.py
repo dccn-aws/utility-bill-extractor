880104c833301bdeb7518d0459cde8ea233c7b10
dccn-aws <dccn@amazon.com>
2022-09-19 01:26:17 +0000

    major refactoring and bugfixing pass. Integrated with carbon workshop now to read from raw data bucket and output to transformed data bucket

--- lib/lambda/utility-bill-extractor.py
+++ lib/lambda/utility-bill-extractor.py
@@ -1,10 +1,10 @@
+import re
 import json
 import time
-import urllib.parse
 import boto3
 import logging
-import re
 from datetime import datetime
+
 
 LOGGER = logging.getLogger()
 LOGGER.setLevel(logging.INFO)
@@ -19,9 +19,11 @@ awslambda = boto3.client('lambda')
 RAW_DATA_BUCKET = 'carbonlake-bills-storage'
 TRANSFORMED_DATA_BUCKET = 'carbonlake-transformed-data'
 
+
 def query_Textract(bills, query):
-    # Event is lambda event
+    # bills is a list of keys in S3 that are our bills to process
     # query is a QueriesConfig object for textract.start_document_analysis()
+    # returns a list of Textract jobs to be be retrieved when complete
     jobs = []
     for bill in bills:
         textract_response = textract.start_document_analysis(
@@ -41,45 +43,52 @@ def query_Textract(bills, query):
     
     LOGGER.info('Querying {0} bills in Textract'.format(len(jobs)))
     return jobs
-    
-def unpack_query(results):
-    i = 0
-    data = {}
-    while i < len(results):
-        data[results[i]['Query']['Alias']] = results[i+1]['Text']
-        i = i+2
-    
-    # Try block to catch multiple datetime formats, you may need to add more here
-    for schema in ['%B %d,%Y', '%B %d, %Y']:
-        try:
-            data['bill_date'] = datetime.strptime(data['bill_date'],schema).strftime('%Y-%m-%d')
-        except:
-            pass
-        
-    return data
-    
+
+
 def retrieve_Textract_query_results(jobids):
+    # jobids is a list of pending Textract document analysis job IDs
+    # returns a clean dict object of each query and returned result
+    
     results = []
     for job in jobids:
         textract_response = textract.get_document_analysis(JobId=job)
         while(textract_response["JobStatus"] == "IN_PROGRESS"):
-            time.sleep(1) if textract_response["JobStatus"] == "IN_PROGRESS"
+            time.sleep(1)
             textract_response = textract.get_document_analysis(JobId=job)
     
         lookup = ["QUERY", "QUERY_RESULT"]
         
+        # one line filter function to just have queries and results to process
         queryResults = [b for b in textract_response["Blocks"] if b['BlockType'] in lookup]
         
         results.append(unpack_query(queryResults))
     
     LOGGER.info('Successfully retrieved {0} queries from Textract'.format(len(jobids)))
     return results
+
+
+def unpack_query(results):
+    # Helper function for retrieve_Textract_query_results
+    # unpacks a list of queries and responses from Textract API
     
-def get_bills(bucket):
+    i = 0
+    data = {}
+    while i < len(results):
+        data[results[i]['Query']['Alias']] = results[i+1]['Text']
+        i = i+2
     
-    return bills
-    
+    # Try block to catch multiple datetime formats, you may need to add more formats here
+    for schema in ['%B %d,%Y', '%B %d, %Y']:
+        try:
+            data['bill_date'] = datetime.strptime(data['bill_date'],schema).strftime('%Y-%m-%d') # saves as YYYY-MM-DD
+        except:
+            pass
+        
+    return data
+
+
 def transform_data(data):
+    
     TransformedData = []
     for entry in data:
         event_id = '{0}-{1}-{2}'.format(entry['utility_provider_name'],
@@ -88,34 +97,38 @@ def transform_data(data):
                                         )
         zipcode = re.search("\d{5}\-?\d{0,4}", entry['utility_provider_address']).group()
         activity = awslambda.invoke(FunctionName='GridRegionSelector',
-                                    Payload={'country':'US','zipcode':zipcode},
-                                    RequestType='RequestResponse'
+                                    Payload=json.dumps({'country':'US','zipcode':zipcode}),
+                                    InvocationType='RequestResponse'
                                     )
+        activity = json.loads(activity['Payload'].read().decode())
         TransformedData.append({
             "activity_event_id": event_id,
             "supplier": entry['utility_provider_name'],
             "scope": 2,
             "category": "grid-region-location-based",
-            "activity": entry['utility_provider_name'],
+            "activity": activity['region'],
             "raw_data": entry['kwh_usage'],
             "units": "kwH"
             }
         )
-        
+    LOGGER.info('Successfull transformed {0} bills'.format(len(data)))
     return TransformedData
-    
+
+
 def write_bills_to_s3(bills):
     
     for bill in bills:
-        filename = "{0}.pdf".format(bill['activity_event_id'])
-        with open(filename, "w") as outfile:
-            json.dump(bill, outfile)
-        s3.put_ojbect(Body=filename,
+        filename = "{0}.json".format(bill['activity_event_id'])
+        #with open('/tmp/'+filename, "w") as outfile:
+        #    outfile.write(json.dumps(bill, indent=4))
+        s3.put_object(Body=json.dumps(bill,indent=4),
                       Bucket=TRANSFORMED_DATA_BUCKET,
-                      key='scope2-bill-extracted-data/{0}'.format(filename))
-                      )
-        
-        
+                      Key='scope2-bill-extracted-data/{0}'.format(filename)
+                     )
+        LOGGER.info('Wrote {0} to {1}/scope2-bill-extracted-data/'.format(filename,TRANSFORMED_DATA_BUCKET))
+    
+    LOGGER.info('Wrote {0} bills to {1}/scope2-bill-extracted-data/'.format(len(bills),TRANSFORMED_DATA_BUCKET))
+    return
 
 def lambda_handler(event, context):
     
@@ -158,12 +171,10 @@ def lambda_handler(event, context):
     
     TextractJobs = query_Textract(bills, query)
     
-    query_results = retrieve_Textract_query_results(TextractJobs)
-    
-    ExtractedData = unpack_query(query_results)
+    ExtractedData = retrieve_Textract_query_results(TextractJobs)
     
     TransformedBills = transform_data(ExtractedData)
     
     write_bills_to_s3(TransformedBills)
-        return data
-
+    
+    return {'status':'Success!'}
